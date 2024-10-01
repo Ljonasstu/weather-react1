796bdfc06d5e3df1f889e0240cd8821f942e5da5
Ljonasstu <156952328+Ljonasstu@users.noreply.github.com>
2024-10-01 16:44:10 +0000

    new changes

---
+++ my-weather/src/index.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+
+import Weather from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <Weather />
+    </div>
+  </StrictMode>
+);
